b6ffe0ed3f4b557616ba8c2002c9d4256d52a2e8
oshohoud078 <oshohoud078@PR26.hhs.win.fuhsd.org>
2019-05-10 12:57:43 -0700

    sup

--- ChordIdentifier/src/Interval.java
+++ ChordIdentifier/src/Interval.java
@@ -4,7 +4,7 @@ public class Interval {
 	private int value;
 	private char quality;
 	
-	public static final char DIMINISHED = '°';
+	public static final int DIMINISHED = '°';
 	public static final char MINOR = 'm';
 	public static final char PERFECT = 'P';
 	public static final char MAJOR = 'M';
@@ -16,6 +16,90 @@ public class Interval {
 		if (value == 1) {
 			if (test == 0) {
 				quality = PERFECT;
+			}
+			else if (test == 1) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 2) {
+			if (test == 0) {
+				quality = DIMINISHED;
+			}
+			else if (test == 1) {
+				quality = MINOR;
+			}
+			else if (test == 2) {
+				quality = MAJOR;
+			}
+			else if (test == 3) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 3) {
+			if (test == 2) {
+				quality = DIMINISHED;
+			}
+			else if (test == 3) {
+				quality = MINOR;
+			}
+			else if (test == 4) {
+				quality = MAJOR;
+			}
+			else if (test == 5) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 4) {
+			if (test == 4) {
+				quality = DIMINISHED;
+			}
+			else if (test == 5) {
+				quality = PERFECT;
+			}
+			else if (test == 6) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 5) {
+			if (test == 6) {
+				quality = DIMINISHED;
+			}
+			else if (test == 7) {
+				quality = MINOR;
+			}
+			else if (test == 8) {
+				quality = MAJOR;
+			}
+			else if (test == 3) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 2) {
+			if (test == 0) {
+				quality = DIMINISHED;
+			}
+			else if (test == 1) {
+				quality = MINOR;
+			}
+			else if (test == 2) {
+				quality = MAJOR;
+			}
+			else if (test == 3) {
+				quality = AUGMENTED;
+			}
+		}
+		else if (value == 2) {
+			if (test == 0) {
+				quality = DIMINISHED;
+			}
+			else if (test == 1) {
+				quality = MINOR;
+			}
+			else if (test == 2) {
+				quality = MAJOR;
+			}
+			else if (test == 3) {
+				quality = AUGMENTED;
 			}
 		}
 	}
