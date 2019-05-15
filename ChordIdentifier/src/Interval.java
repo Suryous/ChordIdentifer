d2718b1249e39c5950c25c5ca621b46e137cb49e
oshohoud078 <oshohoud078@PR26.hhs.win.fuhsd.org>
2019-05-15 11:47:23 -0700

    fucl u surya

--- ChordIdentifier/src/Interval.java
+++ ChordIdentifier/src/Interval.java
@@ -4,15 +4,40 @@ public class Interval {
 	private int value;
 	private char quality;
 	
+	
+	/**
+	 * The character indicating a diminished interval
+	 */
 	public static final int DIMINISHED = '°';
+	/**
+	 * The character indicating a minor interval
+	 */
 	public static final char MINOR = 'm';
+	/**
+	 * The character indicating a perfect interval
+	 */
 	public static final char PERFECT = 'P';
+	/**
+	 * The character indicating a major interval
+	 */
 	public static final char MAJOR = 'M';
+	/**
+	 * The character indicating an augmented interval
+	 */
 	public static final char AUGMENTED = '+';
 	
+	/**
+	 * Constructs an Interval with a quality and value based on the interval between two notes.
+	 * @param n1 The lower note to be compared.
+	 * @param n2 The higher note to be compared.
+	 */
 	public Interval(Note n1, Note n2) {
-		value = n2.getCharLetter()-n1.getCharLetter()+1;
-		int test = n2.getValue()-n1.getValue();
+		value = Math.abs(n2.getCharLetter()-n1.getCharLetter())+1;
+		int n2v = n2.getValue();
+		if (n1.getValue() > n2v) {
+			n2v += 12;
+		}
+		int test = Math.abs(n2v - n1.getValue());
 		if (value == 1) {
 			if (test == 0) {
 				quality = PERFECT;
@@ -65,43 +90,47 @@ public class Interval {
 				quality = DIMINISHED;
 			}
 			else if (test == 7) {
-				quality = MINOR;
+				quality = PERFECT;
 			}
 			else if (test == 8) {
-				quality = MAJOR;
-			}
-			else if (test == 3) {
 				quality = AUGMENTED;
 			}
 		}
-		else if (value == 2) {
-			if (test == 0) {
+		else if (value == 6) {
+			if (test == 7) {
 				quality = DIMINISHED;
 			}
-			else if (test == 1) {
+			else if (test == 8) {
 				quality = MINOR;
 			}
-			else if (test == 2) {
+			else if (test == 9) {
 				quality = MAJOR;
 			}
-			else if (test == 3) {
+			else if (test == 10) {
 				quality = AUGMENTED;
 			}
 		}
-		else if (value == 2) {
-			if (test == 0) {
+		else if (value == 7) {
+			if (test == 9) {
 				quality = DIMINISHED;
 			}
-			else if (test == 1) {
+			else if (test == 10) {
 				quality = MINOR;
 			}
-			else if (test == 2) {
+			else if (test == 11) {
 				quality = MAJOR;
 			}
-			else if (test == 3) {
+			else if (test == 0) {
 				quality = AUGMENTED;
 			}
 		}
+	}
+	
+	/**
+	 * @return The interval as a string
+	 */
+	public String toString() {
+		return "" + quality + value;
 	}
 	
 }
--- ChordIdentifier/src/Note.java
+++ ChordIdentifier/src/Note.java
@@ -1,38 +1,117 @@
 
 public class Note {
 	
-	private String name;
+	private String name = "";
 	private int octave;
 	private int val;
 	
+	/**
+	 * The note value indicating an A. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int A = 0;
+	/**
+	 * The note value indicating an A#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int A$ = 1;
+	/**
+	 * The note value indicating a Bb. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Bb = 1;
+	/**
+	 * The note value indicating a B. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int B = 2;
+	/**
+	 * The note value indicating an Cb. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Cb = 2;
+	/**
+	 * The note value indicating a B#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int B$ = 3;
+	/**
+	 * The note value indicating a C. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int C = 3;
+	/**
+	 * The note value indicating a C#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int C$ = 4;
+	/**
+	 * The note value indicating a Db. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Db = 4;
+	/**
+	 * The note value indicating a D. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int D = 5;
+	/**
+	 * The note value indicating a D#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int D$ = 6;
+	/**
+	 * The note value indicating an Eb. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Eb = 6;
+	/**
+	 * The note value indicating an E. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int E = 7;
+	/**
+	 * The note value indicating an Fb. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Fb = 7;
+	/**
+	 * The note value indicating an E#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int E$ = 8;
+	/**
+	 * The note value indicating an F. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int F = 8;
+	/**
+	 * The note value indicating an F#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int F$ = 9;
+	/**
+	 * The note value indicating a Gb. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Gb = 9;
+	/**
+	 * The note value indicating a G. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int G = 10;
+	/**
+	 * The note value indicating a G#. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int G$ = 11;
+	/**
+	 * The note value indicating an Ab. The value is determined by the number of half-steps that the note is up from A.
+	 */
 	public static final int Ab = 11;
 	
+	
+	/**
+	 * The value representing a double-sharp accidental. The value is determined by how the accidental modifies the letter note value.
+	 */
 	public static final int DOUBLE_SHARP = 2;
+	/**
+	 * The value representing a sharp accidental. The value is determined by how the accidental modifies the letter note value.
+	 */
 	public static final int SHARP = 1;
+	/**
+	 * The value representing a flat accidental. The value is determined by how the accidental modifies the letter note value.
+	 */
 	public static final int FLAT = -1;
+	/**
+	 * The value representing a double-flat accidental. The value is determined by how the accidental modifies the letter note value.
+	 */
 	public static final int DOUBLE_FLAT = -2;
 	
-	
+	/**
+	 * Creates a note with a natural accidental.
+	 * @param letter The letter name of the note.
+	 */
 	public Note(String letter) {
 		name += letter.toUpperCase();
 		if (letter.equalsIgnoreCase("A")) {
@@ -59,7 +138,11 @@ public class Note {
 		
 	}
 	
-	
+	/**
+	 * Creates a note.
+	 * @param letter The letter name of the note.
+	 * @param a The accidental of the note.
+	 */
 	public Note(String letter, int a) {
 		name += letter.toUpperCase();
 		val = getValFromLetter(letter) + a;
@@ -79,6 +162,11 @@ public class Note {
 		
 	}
 	
+	/**
+	 * 
+	 * @param letter The letter name to be used.
+	 * @return The half-step note value of a natural note with the given letter name.
+	 */
 	public int getValFromLetter(String letter) {
 		if (letter.equalsIgnoreCase("A")) {
 			return A;
@@ -106,24 +194,44 @@ public class Note {
 		}
 	}
 	
+	/**
+	 * @return The note name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * 
+	 * @return The note letter name as a String
+	 */
 	public String getStringLetter() {
 		return name.substring(0,1);
 	}
 	
+	/**
+	 * 
+	 * @return The note letter name as a character
+	 */
 	public char getCharLetter() {
 		return name.charAt(0);
 	}
 	
+	/**
+	 * 
+	 * @return The number of half-steps that the note is up from A
+	 */
 	public int getValue() {
 		return val;
 	}
 	
+	/**
+	 * 
+	 * @param other The other note to be compared
+	 * @return The interval between this note and other
+	 */
 	public Interval getInterval(Note other) {
-		
+		return new Interval(this, other);
 	}
 	
 	
